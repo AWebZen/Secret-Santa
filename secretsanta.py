e4e7991ae4bcd69d217eb0c442ab8b62ad13fab8
AWebZen <30926788+AWebZen@users.noreply.github.com>
2020-01-08 14:51:35 +0100

    Put example in separate file

--- secretsanta.py
+++ secretsanta.py
@@ -177,29 +177,3 @@ Secret Santa"""
         if not hasattr(self, "results"):
             self.find_secret_match()
         self.send_secret_santa_results(mail_address)
-
-
-if __name__ == '__main__':
-    #==========================================================================
-    #     SMALL EXAMPLE
-    #==========================================================================
-    #Yours or another, after configuration to allow for Python to send mails
-    secret_email = "your-secret-santa-mail@gmail.com" 
-    
-    friends = ["You", "Leonard", "Penny", "Howard", "Raj"]
-    emails = ["mymail@hotmail.com", "leonardsmail@yahoo.com", "pennysmail@gmail.com",
-              "howardsmail@hotchicks.com", "rajsmail@university.com"]
-    exceptions = {fr:[] for fr in friends}
-    #Penny and Leonard are married and are mutually excluded
-    exceptions["Leonard"].append("Penny")
-    exceptions["Penny"].append("Leonard")
-    
-    ss = SecretSanta(friends, emails, exceptions)
-    #Raj does not want to participate anymore
-    ss.remove_people(["Raj"])
-    ss.get_matches() #Who is sending to who?
-    ss.send_secret_santa(secret_email) #Send results
-    
-    #Or simply
-    ss = SecretSanta(friends, emails, exceptions)
-    ss.do_and_send_secret_santa(secret_email)
